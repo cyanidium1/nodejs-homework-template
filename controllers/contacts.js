2916155e5fae85facb684832c01e379ba68f9dc4
Fedir <99473719+cyanidium1@users.noreply.github.com>
2023-09-09 17:32:28 +0200

    boring node

---
+++ controllers/contacts.js
@@ -0,0 +1,66 @@
+const Contact = require("../models/Contact");
+
+const { HttpError } = require("../helpers");
+
+const ctrlWrapper = require("../decorators/ctrlWrapper");
+
+const getAll = async (req, res) => {
+  const result = await Contact.find();
+  res.status(200).json(result);
+};
+
+const getById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findById(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+};
+
+const addContact = async (req, res) => {
+  const result = await Contact.create(req.body);
+  res.status(201).json(result);
+};
+
+const removeContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndRemove({ _id: contactId });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json({
+    message: "contact deleted",
+  });
+};
+
+const updateContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+};
+
+const updateStatusContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+};
+
+module.exports = {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  addContact: ctrlWrapper(addContact),
+  removeContact: ctrlWrapper(removeContact),
+  updateContact: ctrlWrapper(updateContact),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,61 +1,35 @@
 const express = require("express");
-const contacts = require("../../models/contacts");
+
+const ctrl = require("../../controllers/contacts");
+
+const { validateBody } = require("../../decorators");
+
+const { isValidId, isBody } = require("../../middlewares");
+
+const schemas = require("../../schemas/contacts");
+
+const addValidate = validateBody(schemas.addSchema);
+
+const addFavoriteSchema = validateBody(schemas.addFavoriteSchema);
 
 const router = express.Router();
 
-// http://localhost:3000/api/contacts
-router.get("/", async (req, res, next) => {
-  const all = await contacts.getAll();
+router.get("/", ctrl.getAll);
 
-  res.json(all);
-});
+router.get("/:contactId", isValidId, ctrl.getById);
 
-// http://localhost:3000/api/contacts/AeHIrLTr6JkxGE6SN-0Rw
-router.get("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
-  const cnt = await contacts.getById(contactId);
-  if (cnt === null) {
-    res.status(404);
-    res.json({ message: "Not found" });
-    return;
-  }
-  res.json(cnt);
-});
+router.post("/", addValidate, ctrl.addContact);
 
-router.post("/", async (req, res, next) => {
-  const { name, email, phone } = req.body;
-  if (!name || !email || !phone) {
-    res.status(400);
-    res.json({ message: "missing required name field" });
-    return;
-  }
-  const resp = await contacts.add(name, email, phone);
-  res.status(201);
-  res.json(resp);
-});
+router.delete("/:contactId", isValidId, ctrl.removeContact);
 
-router.delete("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
-  const cnt = await contacts.deleteContact(contactId);
-  if (cnt === null) {
-    res.status(404);
-    res.json({ message: "Not found" });
-    return;
-  }
-  res.status(200);
-  res.json({ message: "contact deleted" });
-});
+router.put("/:contactId", isValidId, addValidate, ctrl.updateContact);
 
-router.put("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
-  const { name, email, phone } = req.body;
-  const newCont = contacts.update(contactId, name, email, phone);
-  if (newCont === null) {
-    res.status(404);
-    res.json({ message: "not found" });
-    return;
-  }
-  res.json(newCont);
-});
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  isBody,
+  addFavoriteSchema,
+  ctrl.updateStatusContact
+);
 
 module.exports = router;
